2207ab46605bfe150e5d1c2900f9b55877229903
Juan .C <160873765+reddevx@users.noreply.github.com>
2024-03-18 14:13:26 +0000

    Update age.rb

--- age.rb
+++ age.rb
@@ -1,16 +1,29 @@
-puts "Enter the number of the month in which you were born; "
+result = ""
 
-month = gets.chomp.to_i
+loop do 
+    puts result
+    puts "Select of option:"
+    puts "1- Find out a person's age"
+    puts "0- Exit"
+    print "Option: "
 
-case month 
- when 1..3
-    puts "you were born at the beginning of the year."
- when 9..12
-    puts "you were born at the end of the yea.r"
- when 4..6
-    puts "you were born in the first half of the year."
- when 7..9
-    puts "you were born in the second half of the year."
- else 
-    puts "Can'not defined, try again."
- end
+
+option = gets.chomp.to_i
+
+if option == 1
+    print "Enter your birth year: "
+    year_of_birth = gets.chomp.to_i
+
+    print "Enter the current year: "
+    current_year = gets.chomp.to_i
+    
+    age = current_year - year_of_birth
+    
+    result = "Who was born in #{year_of_birth}, has #{age} in #{current_year}"
+    elsif option == 0
+        break
+    else
+        result = "invalid option."
+    end
+    system "clear"
+end
